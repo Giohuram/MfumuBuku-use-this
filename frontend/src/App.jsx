63f428ec3c70ff4c433ddf53b3bd13fe942637c7
giohuram <giohuram@icloud.com>
2024-04-20 20:05:59 +0100

    installed tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'; 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline">Mbote</p>
+    </>
+  )
+}
+
+export default App
