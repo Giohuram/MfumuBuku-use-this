bc206e1b29eacabcbd5d33f1e03f47bc639dbfdb
giohuram <giohuram@icloud.com>
2024-04-24 14:07:17 +0100

    added morgan dependency

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -2,16 +2,19 @@
 
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const authController = require('./authController');
 
-// Endpoint pour l'inscription
+// Route d'inscription
 router.post('/signup', authController.signup);
 
-// Endpoint pour la connexion
-router.post('/login', authController.login);
+// Route de connexion
+router.post('/login', passport.authenticate('local', { session: false }), authController.login);
 
-// Endpoint pour la déconnexion
-router.get('/logout', authController.logout);
+// Route deconnexion 
+router.post('/logout', (req, res) => {
+    req.logout(); // Utilisez la méthode `logout()` fournie par Passport pour déconnecter l'utilisateur
+    res.status(200).json({ message: 'Déconnexion réussie' });
+  });
 
 module.exports = router;
-
--- server/server.js
+++ server/server.js
@@ -1,18 +1,48 @@
 // server.js
 
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const cors = require('cors');
-const authRoutes = require('./authRoutes');
+const authRoutes = require('./authRoutes'); // Importez vos routes d'authentification
 
 const app = express();
 
-app.use(express.json());
+// Middleware CORS
 app.use(cors());
 
-app.use('/auth', authRoutes);
+// Middleware pour le parsing des données JSON
+app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+// Initialiser Passport
+app.use(passport.initialize());
+
+// Configuration de la stratégie locale
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { username: username }
+      });
+      if (!user) {
+        return done(null, false);
+      }
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
+
+// Utiliser les routes d'authentification
+app.use('/auth', authRoutes); // Utilisez le préfixe '/auth' pour toutes les routes d'authentification
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
-
