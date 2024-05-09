92edbcf9a74f1e05b584348083f4229f3e011c94
giohuram <giohuram@icloud.com>
2024-05-09 16:19:13 +0100

    added pro-types to help for my code maintainability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
@@ -20,24 +19,29 @@ function App() {
 
   return (
     <Router>
-      <BookProvider>
-        <>
-          {!(window.location.pathname.includes('/librairie') || window.location.pathname.includes('/MyLibrary') || window.location.pathname.includes('/MyBooks') || window.location.pathname.includes('/ReadingClub') || window.location.pathname.includes('/Subscription') || window.location.pathname.includes('/MyProfile')) && <NavBar isLoggedIn={isLoggedIn} />}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/librairie" element={isLoggedIn ? <Librairie /> : <Navigate to="/login" />} />
-            <Route path="/MyLibrary" element={<MyLibrary />} />
-            <Route path="/MyBooks" element={<MyBooks />} />
-            <Route path="/ReadingClub" element={<ReadingClub />} />
-            <Route path="/Subscription" element={<Subscription />} />
-            <Route path="/MyProfile" element={<MyProfile />} />
-            <Route path="/book/:id" element={<BookPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </>
-      </BookProvider>
+      <>
+        {!(window.location.pathname.includes('/librairie') || window.location.pathname.includes('/MyLibrary') || window.location.pathname.includes('/MyBooks') || window.location.pathname.includes('/ReadingClub') || window.location.pathname.includes('/Subscription') || window.location.pathname.includes('/book/:id') || window.location.pathname.includes('/MyProfile')) && <NavBar isLoggedIn={isLoggedIn} />}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/librairie" element={isLoggedIn ? <Librairie /> : <Navigate to="/login" />} />
+          <Route
+            path="/MyLibrary"
+            element={
+              <BookProvider>
+                <MyLibrary />
+              </BookProvider>
+            }
+          />
+          <Route path="/MyBooks" element={<MyBooks />} />
+          <Route path="/ReadingClub" element={<ReadingClub />} />
+          <Route path="/Subscription" element={<Subscription />} />
+          <Route path="/MyProfile" element={<MyProfile />} />
+          <Route path="/book/:id" element={<BookPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </>
     </Router>
   );
 }
