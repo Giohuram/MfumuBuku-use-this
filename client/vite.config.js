f57fe79d1cd1ea8a82dad923108b00c7355fc2e0
giohuram <giohuram@icloud.com>
2024-05-19 23:10:11 +0100

    restart build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change 'dist' to 'build'
+  },
   server: {
     proxy: {
       '/logout': {
@@ -12,7 +16,5 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    outDir: 'build' // Spécifiez ici le répertoire de publication souhaité
-  }
 });
+
