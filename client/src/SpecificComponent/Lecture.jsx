489dcfa65d381880da3596c149bbc7fb1ecc1344
giohuram <giohuram@icloud.com>
2024-05-20 07:34:29 +0100

    improved LibrairieButon in the Lecture component

--- client/src/SpecificComponent/Lecture.jsx
+++ client/src/SpecificComponent/Lecture.jsx
@@ -1,5 +1,7 @@
+// Affiche la page de lecture du livre sélectionné
+
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; // Importez PropTypes depuis la bibliothèque prop-types
 import { ReactReader } from 'react-reader';
 import { useLocation } from 'react-router-dom';
 import LibrairieNavBar from '../components/LibrairieNavBar';
@@ -7,35 +9,33 @@ import LibrairieButton from '../SharedComponents/LibrairieButton';
 
 const Lecture = () => {
   const location = useLocation();
-  const book = location.state?.book;
+  const book = location.state?.book; // Access book from state, handle potential absence
 
   if (!book) {
     return <div>Loading...</div>;
   }
 
   return (
-    <>
-      <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-        <LibrairieNavBar />
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <ReactReader
-            url={book.content}
-            location={0}
-            locationChanged={(epubcfi) => console.log(epubcfi)}
-          />
-        </div>
-      </div>
-      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+    <div style={{ height: '100vh' }}>
+      <LibrairieNavBar />
+      <ReactReader
+        url={book.content}
+        location={0}
+        locationChanged={(epubcfi) => console.log(epubcfi)}
+      />
+       <div style={{ marginTop: '20px', textAlign: 'center' }}>
         <LibrairieButton />
       </div>
-    </>
+    </div>
   );
 };
 
+// Définissez les types de props attendus pour le composant BookReader
 Lecture.propTypes = {
   book: PropTypes.shape({
-    bookContentURL: PropTypes.string.isRequired,
-  }).isRequired,
+    bookContentURL: PropTypes.string.isRequired, // Définissez le type et assurez-vous qu'il est requis
+    // Ajoutez d'autres propriétés ici si nécessaire
+  }).isRequired, // Assurez-vous que book est requis
 };
 
-export default Lecture;
+export default Lecture
