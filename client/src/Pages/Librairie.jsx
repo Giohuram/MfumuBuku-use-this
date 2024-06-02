3a58c1117868e4da689bc955a95a1f9566ea2080
giohuram <giohuram@icloud.com>
2024-06-02 18:17:57 +0100

    refactor(Librairie & BookCard): ensure they both help pass books to MyBooks component when a user clicks on ajouter a ma collection button

--- client/src/Pages/Librairie.jsx
+++ client/src/Pages/Librairie.jsx
@@ -1,18 +1,125 @@
-import { useState, useEffect, useContext } from 'react';
+// import { useState, useEffect, useContext } from 'react';
+// import axios from 'axios';
+// import LibrairieNavBar from '../components/LibrairieNavBar';
+// import BookCard from '../SharedComponents/BookCard';
+// import { useBookContext } from '../Context/BookContext';
+// import Banner from '../SharedComponents/Banner';
+// import { UserContext } from '../Context/userContext';
+
+// const Librairie = () => {
+//   const { books, addBookToLibrary } = useBookContext();
+//   const [booksByCategory, setBooksByCategory] = useState({});
+//   const [filteredBooks, setFilteredBooks] = useState([]);
+//   const [filteredCategories, setFilteredCategories] = useState({});
+//   const { addToMyBooks } = useContext(UserContext);
+//   const [isLoading, setIsLoading] = useState(false); // Added state for loading
+
+//   useEffect(() => {
+//     fetchBooks();
+//   }, []);
+
+//   useEffect(() => {
+//     setFilteredBooks(books);
+//   }, [books]);
+
+//   useEffect(() => {
+//     filterCategories();
+//   }, [filteredBooks, booksByCategory]); // Optimized dependency array
+
+//   const fetchBooks = async () => {
+//     setIsLoading(true); // Set loading state to true
+//     try {
+//       const token = localStorage.getItem('token');
+//       const response = await axios.get('https://mfumubuku-kids.onrender.com/Book', {
+//         headers: {
+//           Authorization: `Bearer ${token}`
+//         }
+//       });
+//       const groupedBooks = groupBooksByCategory(response.data);
+//       setBooksByCategory(groupedBooks);
+//       setFilteredBooks(response.data);
+//     } catch (error) {
+//       console.error('Error fetching books:', error);
+//       // Handle error by displaying a message to the user
+//     } finally {
+//       setIsLoading(false); // Set loading state to false
+//     }
+//   };
+
+//   const groupBooksByCategory = (books) => {
+//     const groupedBooks = {};
+//     books.forEach(book => {
+//       if (!groupedBooks[book.category]) {
+//         groupedBooks[book.category] = [];
+//       }
+//       groupedBooks[book.category].push(book);
+//     });
+//     return groupedBooks;
+//   };
+
+//   const handleAddToCollection = (book) => {
+//     addBookToLibrary(book);
+//     addToMyBooks(book.id);
+//   };
+
+//   const filterCategories = () => {
+//     const newFilteredCategories = {};
+//     for (const category in booksByCategory) {
+//       const filteredBooksInCategory = booksByCategory[category].filter(book =>
+//         filteredBooks.some(filteredBook => filteredBook.id === book.id)
+//       );
+//       if (filteredBooksInCategory.length > 0) {
+//         newFilteredCategories[category] = filteredBooksInCategory;
+//       }
+//     }
+//     setFilteredCategories(newFilteredCategories);
+//   };
+
+//   const displayNoBooksMessage = () => (
+//     <div className='mt-5 ml-20 text-2xl font-semibold'>No books available</div>
+//   );
+
+//   return (
+//     <div>
+//       <LibrairieNavBar />
+//       <Banner books={filteredBooks} setFilteredBooks={setFilteredBooks} />
+//       {isLoading ? (
+//         // Display loading indicator while fetching books
+//         <div>Chargement des livres...</div>
+//       ) : (
+//         <div>
+//           {Object.keys(filteredCategories).length > 0 ? (
+//             Object.keys(filteredCategories).map(category => (
+//               <div key={category}>
+//                 <h2 className='mt-5 ml-20 text-2xl font-semibold'>{category}</h2>
+//                 <div className="ml-[-0px] mr-[-0px]">
+//                   <BookCard books={filteredCategories[category]} onAddToCollection={handleAddToCollection} />
+//                 </div>
+//               </div>
+//             ))
+//           ) : (
+//             displayNoBooksMessage()
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Librairie;
+
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import LibrairieNavBar from '../components/LibrairieNavBar';
 import BookCard from '../SharedComponents/BookCard';
 import { useBookContext } from '../Context/BookContext';
 import Banner from '../SharedComponents/Banner';
-import { UserContext } from '../Context/userContext';
 
 const Librairie = () => {
   const { books, addBookToLibrary } = useBookContext();
   const [booksByCategory, setBooksByCategory] = useState({});
   const [filteredBooks, setFilteredBooks] = useState([]);
   const [filteredCategories, setFilteredCategories] = useState({});
-  const { addToMyBooks } = useContext(UserContext);
-  const [isLoading, setIsLoading] = useState(false); // Added state for loading
 
   useEffect(() => {
     fetchBooks();
@@ -24,10 +131,9 @@ const Librairie = () => {
 
   useEffect(() => {
     filterCategories();
-  }, [filteredBooks, booksByCategory]); // Optimized dependency array
+  }, [filteredBooks]);
 
   const fetchBooks = async () => {
-    setIsLoading(true); // Set loading state to true
     try {
       const token = localStorage.getItem('token');
       const response = await axios.get('https://mfumubuku-kids.onrender.com/Book', {
@@ -40,9 +146,6 @@ const Librairie = () => {
       setFilteredBooks(response.data);
     } catch (error) {
       console.error('Error fetching books:', error);
-      // Handle error by displaying a message to the user
-    } finally {
-      setIsLoading(false); // Set loading state to false
     }
   };
 
@@ -59,7 +162,6 @@ const Librairie = () => {
 
   const handleAddToCollection = (book) => {
     addBookToLibrary(book);
-    addToMyBooks(book.id);
   };
 
   const filterCategories = () => {
@@ -74,34 +176,27 @@ const Librairie = () => {
     }
     setFilteredCategories(newFilteredCategories);
   };
-
-  const displayNoBooksMessage = () => (
-    <div className='mt-5 ml-20 text-2xl font-semibold'>No books available</div>
-  );
+  
 
   return (
     <div>
       <LibrairieNavBar />
       <Banner books={filteredBooks} setFilteredBooks={setFilteredBooks} />
-      {isLoading ? (
-        // Display loading indicator while fetching books
-        <div>Chargement des livres...</div>
-      ) : (
-        <div>
-          {Object.keys(filteredCategories).length > 0 ? (
-            Object.keys(filteredCategories).map(category => (
-              <div key={category}>
-                <h2 className='mt-5 ml-20 text-2xl font-semibold'>{category}</h2>
-                <div className="ml-[-0px] mr-[-0px]">
-                  <BookCard books={filteredCategories[category]} onAddToCollection={handleAddToCollection} />
-                </div>
+      
+      <div>
+        {Object.keys(filteredCategories).length > 0 ? (
+          Object.keys(filteredCategories).map(category => (
+            <div key={category}>
+              <h2 className='mt-5 ml-20 text-2xl font-semibold'>{category}</h2>
+              <div className="ml-[-0px] mr-[-0px]">
+                <BookCard books={filteredCategories[category]} onAddToCollection={handleAddToCollection} />
               </div>
-            ))
-          ) : (
-            displayNoBooksMessage()
-          )}
-        </div>
-      )}
+            </div>
+          ))
+        ) : (
+          <div className='mt-5 ml-20 text-2xl font-semibold'>No books available</div>
+        )}
+      </div>
     </div>
   );
 };
--- client/src/SharedComponents/BookCard.jsx
+++ client/src/SharedComponents/BookCard.jsx
@@ -1,18 +1,91 @@
-import React, { useContext } from 'react';
+// import React, { useContext } from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Pagination } from 'swiper/modules';
+// import { useUserContext } from '../Context/userContext';
+
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+
+// const BookCard = ({ headline, books, onAddToCollection }) => {
+//   const { user, addToMyBooks } = useUserContext(); 
+
+//   const handleAddToCollection = (book) => {
+//     onAddToCollection(book); // This could be used for additional actions
+//     addToMyBooks(book.id); // Use addToMyBooks from UserContext
+//   };
+
+//   return (
+//     <>
+//       <div className='px-4 lg:px-24 mb-2 mt-4'>
+//         <h2 className='text-2xl font-bold text-[#DC7211] mt-5 mb-2'>{headline}</h2>
+//       </div>
+
+//       <div className='px-4 lg:px-24 pb-10'>
+//         <Swiper
+//           slidesPerView={1}
+//           spaceBetween={10}
+//           pagination={{ clickable: true }}
+//           breakpoints={{
+//             640: { slidesPerView: 2, spaceBetween: 10 },
+//             768: { slidesPerView: 3, spaceBetween: 10 },
+//             1024: { slidesPerView: 4, spaceBetween: 10 },
+//           }}
+//           modules={[Pagination]}
+//           className="mySwiper w-full h-full"
+//         >
+//           {books.map(book => (
+//             <SwiperSlide key={book.id}>
+//               <div className="flex flex-col items-center">
+//                 <div>
+//                   <img src={book.bookCover} alt="bookCover" className="mb-2 w-full max-w-[200px] h-auto" />
+//                   <p className="text-center font-semibold">{book.title}</p>
+//                   <p className="text-center font-semibold">Âge concerné: {book.age} ans</p>
+//                 </div>
+//                 <div className="text-center">
+//                   <button onClick={() => handleAddToCollection(book)} className='bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2'>
+//                     Ajouter à ma collection
+//                   </button>
+//                   {/* {addedMessage && <p>{addedMessage}</p>} */}
+//                 </div>      
+//               </div>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default BookCard;
+
+import React, { useContext, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-import { useUserContext } from '../Context/userContext';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../Context/userContext';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 const BookCard = ({ headline, books, onAddToCollection }) => {
-  const { user, addToMyBooks } = useUserContext(); 
+  const { user, addToMyBooks } = useContext(UserContext);
+  // const [selectedBook, setSelectedBook] = useState(null);
+  // const navigate = useNavigate();
 
-  const handleAddToCollection = (book) => {
-    onAddToCollection(book); // This could be used for additional actions
-    addToMyBooks(book.id); // Use addToMyBooks from UserContext
+  // Function to check if the book is already in favorites
+  const isFavorite = (bookId) => {
+    return user && user.books && user.books.some(favoriteBook => favoriteBook.id === bookId);
+  };
+
+  if (!books || books.length === 0) {
+    return <div>No books available</div>;
+  }
+
+  const addToMyCollection = (book) => {
+     onAddToCollection(book);
+     addToMyBooks(book.id);
   };
 
   return (
@@ -24,12 +97,23 @@ const BookCard = ({ headline, books, onAddToCollection }) => {
       <div className='px-4 lg:px-24 pb-10'>
         <Swiper
           slidesPerView={1}
-          spaceBetween={10}
-          pagination={{ clickable: true }}
+          spaceBetween={10} // Adjusted space between slides
+          pagination={{
+            clickable: true,
+          }}
           breakpoints={{
-            640: { slidesPerView: 2, spaceBetween: 10 },
-            768: { slidesPerView: 3, spaceBetween: 10 },
-            1024: { slidesPerView: 4, spaceBetween: 10 },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 10, // Adjusted space between slides for smaller screens
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 10,
+            },
           }}
           modules={[Pagination]}
           className="mySwiper w-full h-full"
@@ -37,16 +121,18 @@ const BookCard = ({ headline, books, onAddToCollection }) => {
           {books.map(book => (
             <SwiperSlide key={book.id}>
               <div className="flex flex-col items-center">
-                <div>
+                <Link to={`/book/${book.id}`} className="flex flex-col items-center">
                   <img src={book.bookCover} alt="bookCover" className="mb-2 w-full max-w-[200px] h-auto" />
                   <p className="text-center font-semibold">{book.title}</p>
                   <p className="text-center font-semibold">Âge concerné: {book.age} ans</p>
-                </div>
+                </Link>
                 <div className="text-center">
-                  <button onClick={() => handleAddToCollection(book)} className='bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2'>
-                    Ajouter à ma collection
-                  </button>
-                  {/* {addedMessage && <p>{addedMessage}</p>} */}
+                      <button
+                        className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2 mr-2"
+                        onClick={() => addToMyCollection(book)}
+                      >
+                        Ajouter à ma collection 
+                      </button>
                 </div>      
               </div>
             </SwiperSlide>
