869bf5fb8fcea706c14f3470c15dfb1410438be6
giohuram <giohuram@icloud.com>
2024-06-02 18:22:33 +0100

    refactor(Librairie & BookCard): ensure they both help pass books to MyBooks component when a user clicks on ajouter a ma collection button 2

--- client/src/LibraryPages/MyBooks.jsx
+++ client/src/LibraryPages/MyBooks.jsx
@@ -1,4 +1,104 @@
-import React, { useContext, useEffect, useState } from 'react';
+// import React, { useContext, useEffect, useState } from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Pagination } from 'swiper/modules';
+// import { useNavigate } from 'react-router-dom';
+// import { useBookContext } from '../Context/BookContext';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import LibrairieNavBar from '../components/LibrairieNavBar';
+// import { UserContext } from '../Context/userContext';
+
+// const MyBooks = () => {
+//   const { books } = useBookContext(); // Assurez-vous que useBookContext fournit tous les livres disponibles
+//   const { user, removeFromMyBooks } = useContext(UserContext);
+//   const navigate = useNavigate();
+//   const [groupedBooks, setGroupedBooks] = useState({});
+
+//   useEffect(() => {
+//     const myBooksDetails = user.myBooks.map(bookId => books.find(book => book.id === bookId));
+//     setGroupedBooks(groupBooksByCategory(myBooksDetails));
+//   }, [user.myBooks, books]);
+
+//   const groupBooksByCategory = (books) => {
+//     const groupedBooks = {};
+//     books.forEach(book => {
+//       if (!groupedBooks[book.category]) {
+//         groupedBooks[book.category] = [];
+//       }
+//       groupedBooks[book.category].push(book);
+//     });
+//     return groupedBooks;
+//   };
+
+//   const handleReadButtonClick = (book) => {
+//     navigate('/Lecture', { state: { book } });
+//   };
+
+//   const handleListenButtonClick = (book) => {
+//     navigate('/LectureAudio', { state: { book } });
+//   };
+
+//   return (
+//     <div>
+//       <LibrairieNavBar />
+//       <div className="mt-10 ml-2 mr-2">
+//         {Object.keys(groupedBooks).length > 0 ? (
+//           Object.keys(groupedBooks).map((category) => (
+//             <div key={category}>
+//               <h2 className='text-2xl font-semibold'>{category}</h2>
+//               <Swiper
+//                 slidesPerView={1}
+//                 spaceBetween={10}
+//                 pagination={{ clickable: true }}
+//                 breakpoints={{
+//                   640: { slidesPerView: 2, spaceBetween: 20 },
+//                   768: { slidesPerView: 4, spaceBetween: 40 },
+//                   1024: { slidesPerView: 5, spaceBetween: 50 },
+//                 }}
+//                 modules={[Pagination]}
+//                 className="mySwiper w-full h-full"
+//               >
+//                 {groupedBooks[category].map((book, index) => (
+//                   <SwiperSlide key={index}>
+//                     <div className="flex flex-col items-center">
+//                       <img src={book.bookCover} alt="bookCover" className="mb-2" />
+//                       <div className="text-center">
+//                         <button
+//                           className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2 mr-2"
+//                           onClick={() => handleReadButtonClick(book)}
+//                         >
+//                           Lire
+//                         </button>
+//                         <button
+//                           className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2"
+//                           onClick={() => handleListenButtonClick(book)}
+//                         >
+//                           Écouter
+//                         </button>
+//                         <button
+//                           className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2"
+//                           onClick={() => removeFromMyBooks(book.id)}
+//                         >
+//                           Supprimer
+//                         </button>
+//                       </div>
+//                     </div>
+//                   </SwiperSlide>
+//                 ))}
+//               </Swiper>
+//             </div>
+//           ))
+//         ) : (
+//           <div className='mt-5 ml-20 text-2xl font-semibold'>No books in your collection</div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MyBooks;
+
+import React, { useContext, useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import { useNavigate } from 'react-router-dom';
@@ -7,90 +107,90 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import LibrairieNavBar from '../components/LibrairieNavBar';
 import { UserContext } from '../Context/userContext';
+import axios from 'axios'; // Importez axios ici
 
 const MyBooks = () => {
-  const { books } = useBookContext(); // Assurez-vous que useBookContext fournit tous les livres disponibles
+  const { groupedBooks } = useBookContext();
   const { user, removeFromMyBooks } = useContext(UserContext);
   const navigate = useNavigate();
-  const [groupedBooks, setGroupedBooks] = useState({});
-
-  useEffect(() => {
-    const myBooksDetails = user.myBooks.map(bookId => books.find(book => book.id === bookId));
-    setGroupedBooks(groupBooksByCategory(myBooksDetails));
-  }, [user.myBooks, books]);
-
-  const groupBooksByCategory = (books) => {
-    const groupedBooks = {};
-    books.forEach(book => {
-      if (!groupedBooks[book.category]) {
-        groupedBooks[book.category] = [];
-      }
-      groupedBooks[book.category].push(book);
-    });
-    return groupedBooks;
-  };
+  const [selectedBook, setSelectedBook] = useState(null);
 
   const handleReadButtonClick = (book) => {
+    setSelectedBook(book);
     navigate('/Lecture', { state: { book } });
   };
 
   const handleListenButtonClick = (book) => {
+    setSelectedBook(book);
     navigate('/LectureAudio', { state: { book } });
   };
+
+  // const handleReturnBook = async (bookId) => {
+  //   try {
+  //     // Récupération du token depuis le contexte utilisateur
+  //     const token = localStorage.getItem('token');
+  //     // Configuration du header avec le token JWT
+  //     const headers = {
+  //       Authorization: `Bearer ${token}`,
+  //     };
+
+  //     // Appel axios avec le header
+  //     await axios.delete(`/user/${user.id}/favorites/${bookId}`, { headers });
+  //     removeFromMyBooks(bookId);
+  //   } catch (error) {
+  //     console.error('Error removing book from favorites:', error);
+  //   }
+  // };
 
   return (
     <div>
       <LibrairieNavBar />
       <div className="mt-10 ml-2 mr-2">
-        {Object.keys(groupedBooks).length > 0 ? (
-          Object.keys(groupedBooks).map((category) => (
-            <div key={category}>
-              <h2 className='text-2xl font-semibold'>{category}</h2>
-              <Swiper
-                slidesPerView={1}
-                spaceBetween={10}
-                pagination={{ clickable: true }}
-                breakpoints={{
-                  640: { slidesPerView: 2, spaceBetween: 20 },
-                  768: { slidesPerView: 4, spaceBetween: 40 },
-                  1024: { slidesPerView: 5, spaceBetween: 50 },
-                }}
-                modules={[Pagination]}
-                className="mySwiper w-full h-full"
-              >
-                {groupedBooks[category].map((book, index) => (
-                  <SwiperSlide key={index}>
-                    <div className="flex flex-col items-center">
-                      <img src={book.bookCover} alt="bookCover" className="mb-2" />
-                      <div className="text-center">
-                        <button
-                          className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2 mr-2"
-                          onClick={() => handleReadButtonClick(book)}
-                        >
-                          Lire
-                        </button>
-                        <button
-                          className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2"
-                          onClick={() => handleListenButtonClick(book)}
-                        >
-                          Écouter
-                        </button>
-                        <button
-                          className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2"
-                          onClick={() => removeFromMyBooks(book.id)}
-                        >
-                          Supprimer
-                        </button>
-                      </div>
+        {Object.keys(groupedBooks).map((category) => (
+          <div key={category}>
+            <h2 className='text-2xl font-semibold'>{category}</h2>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{ clickable: true }}
+              breakpoints={{
+                640: { slidesPerView: 2, spaceBetween: 20 },
+                768: { slidesPerView: 4, spaceBetween: 40 },
+                1024: { slidesPerView: 5, spaceBetween: 50 },
+              }}
+              modules={[Pagination]}
+              className="mySwiper w-full h-full"
+            >
+              {groupedBooks[category].map((book, index) => (
+                <SwiperSlide key={index}>
+                  <div className="flex flex-col items-center">
+                    <img src={book.bookCover} alt="bookCover" className="mb-2" />
+                    <div className="text-center">
+                      <button
+                        className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2 mr-2"
+                        onClick={() => handleReadButtonClick(book)}
+                      >
+                        Lire
+                      </button>
+                      <button
+                        className="bg-[#DC7211] text-white py-2 px-4 rounded-lg mt-2"
+                        onClick={() => handleListenButtonClick(book)}
+                      >
+                        Écouter
+                      </button>
+                      {/* <button
+                        className="mt-2 py-2 px-4 rounded-lg bg-red-500 text-white"
+                        onClick={() => handleReturnBook(book.id)}
+                      >
+                        Retourner ce livre
+                      </button> */}
                     </div>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          ))
-        ) : (
-          <div className='mt-5 ml-20 text-2xl font-semibold'>No books in your collection</div>
-        )}
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        ))}
       </div>
     </div>
   );
